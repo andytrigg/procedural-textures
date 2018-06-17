57bbe4d03842b5922539eff1ccbde4cbe3d00241
Andy Trigg <gigantiqandy@gmail.com>
2018-06-17 16:46:01 +1000

    Introduced a cosine remapping of linear interpolation so that we can generate smooth curves

--- NoiseProcedures.Test/InterpolationTest.cs
+++ NoiseProcedures.Test/InterpolationTest.cs
@@ -34,5 +34,21 @@ namespace NoiseProcedures
         {
             Assert.AreEqual(2.25f, Interpolation.Linear(0, 3, 0.75F));
         }
+
+        [TestCase(0F, 1F, 0f, ExpectedResult = 0.0f)]
+        [TestCase(0F, 1F, 0.1f, ExpectedResult = 0.024471743f)]
+        [TestCase(0F, 1F, 0.2f, ExpectedResult = 0.0954915062f)]
+        [TestCase(0F, 1F, 0.3f, ExpectedResult = 0.206107393f)]
+        [TestCase(0F, 1F, 0.4f, ExpectedResult = 0.345491499f)]
+        [TestCase(0F, 1F, 0.5f, ExpectedResult = 0.5f)]
+        [TestCase(0F, 1F, 0.6f, ExpectedResult = 0.654508531f)]
+        [TestCase(0F, 1F, 0.7f, ExpectedResult = 0.793892622f)]
+        [TestCase(0F, 1F, 0.8f, ExpectedResult = 0.904508531f)]
+        [TestCase(0F, 1F, 0.9f, ExpectedResult = 0.97552824f)]
+        [TestCase(0F, 1F, 1.0f, ExpectedResult = 1.0f)]
+        public float ValueNoiseShouldEvaluateForEachCalculatedPoint(float lo, float high, float alpha)
+        {
+            return Interpolation.CosineRemap(lo, high, alpha);
+        }
     }
 }
--- NoiseProcedures/Interpolation.cs
+++ NoiseProcedures/Interpolation.cs
@@ -13,6 +13,23 @@ namespace NoiseProcedures
         /// <param name="alpha">The fraction between the points whose value will be interoplated based on a linear function. This value should be between 0.0 and 1.0</param>
         /// <returns>The linearly interpolated value.</returns>
         public static float Linear(float low, float high, float alpha) => (1 - alpha) * low + alpha * high;
+
+
+        /// <summary>
+        /// Apply as Cosine remap to a linear interpolation function. 
+        /// This well ensure that the nise function is curved rather than having hard edges where the straight lines from linear interpolation would meet.
+        /// </summary>
+        /// <param name="low">The lowest value of the two points</param>
+        /// <param name="high">The high value of the two points</param>
+        /// <param name="alpha">The fraction between the points whose value will be interoplated based on a linear function. This value should be between 0.0 and 1.0</param>
+        /// <returns></returns>
+        public static float CosineRemap(float low, float high, float alpha)
+        {
+            Debug.Assert(alpha >= 0.0f && alpha <= 1.0f);
+           
+            double alphaRemapCosine = (1 - Math.Cos(alpha * Math.PI)) * 0.5;
+            return Linear(low, high, (float) alphaRemapCosine);
+        }
     }
 
     public class ValueNoise1D
